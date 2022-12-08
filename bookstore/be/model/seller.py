c5b5b5bdeecca6606f9fba1371930e9f34153b6c
Liao <86995945+duliaojason@users.noreply.github.com>
2022-12-08 12:59:12 +0800

    Add files via upload

--- bookstore/be/model/seller.py
+++ bookstore/be/model/seller.py
@@ -1,72 +1,15 @@
 import sqlite3 as sqlite
 from be.model import error
-from be.model.db import DataBase
-from be.model.db import StoreBook1, Store1, StoreUser1, Order1
+from be.model.db import DB
+from be.model.db import StoreBook, Store, StoreUser, Order, User
 
 
-# class Seller(db_conn.DBConn):
-#
-#     def __init__(self):
-#         db_conn.DBConn.__init__(self)
-#
-#     def add_book(self, user_id: str, store_id: str, book_id: str, book_json_str: str, stock_level: int):
-#         try:
-#             if not self.user_id_exist(user_id):
-#                 return error.error_non_exist_user_id(user_id)
-#             if not self.store_id_exist(store_id):
-#                 return error.error_non_exist_store_id(store_id)
-#             if self.book_id_exist(store_id, book_id):
-#                 return error.error_exist_book_id(book_id)
-#
-#             self.conn.execute("INSERT into store(store_id, book_id, book_info, stock_level)"
-#                               "VALUES (?, ?, ?, ?)", (store_id, book_id, book_json_str, stock_level))
-#             self.conn.commit()
-#         except sqlite.Error as e:
-#             return 528, "{}".format(str(e))
-#         except BaseException as e:
-#             return 530, "{}".format(str(e))
-#         return 200, "ok"
-#
-#     def add_stock_level(self, user_id: str, store_id: str, book_id: str, add_stock_level: int):
-#         try:
-#             if not self.user_id_exist(user_id):
-#                 return error.error_non_exist_user_id(user_id)
-#             if not self.store_id_exist(store_id):
-#                 return error.error_non_exist_store_id(store_id)
-#             if not self.book_id_exist(store_id, book_id):
-#                 return error.error_non_exist_book_id(book_id)
-#
-#             self.conn.execute("UPDATE store SET stock_level = stock_level + ? "
-#                               "WHERE store_id = ? AND book_id = ?", (add_stock_level, store_id, book_id))
-#             self.conn.commit()
-#         except sqlite.Error as e:
-#             return 528, "{}".format(str(e))
-#         except BaseException as e:
-#             return 530, "{}".format(str(e))
-#         return 200, "ok"
-#
-#     def create_store(self, user_id: str, store_id: str) -> (int, str):
-#         try:
-#             if not self.user_id_exist(user_id):
-#                 return error.error_non_exist_user_id(user_id)
-#             if self.store_id_exist(store_id):
-#                 return error.error_exist_store_id(store_id)
-#             self.conn.execute("INSERT into user_store(store_id, user_id)"
-#                               "VALUES (?, ?)", (store_id, user_id))
-#             self.conn.commit()
-#         except sqlite.Error as e:
-#             return 528, "{}".format(str(e))
-#         except BaseException as e:
-#             return 530, "{}".format(str(e))
-#         return 200, "ok"
-
-#定义一个卖家，方便随时调试测试
-class mySeller(DataBase):
+# 测试用，最后会代替下面的Seller
+class mySeller(DB):
 
     def __init__(self):
-        DataBase.__init__(self)
+        DB.__init__(self)
 
-#添加书本
     def add_book(self, user_id: str, store_id: str, book_id: str, book_json_str: str, stock_level: int):
         try:
             if not self.user_id_exist(user_id):
@@ -76,7 +19,7 @@ class mySeller(DataBase):
             if self.book_id_exist(store_id, book_id):
                 return error.error_exist_book_id(book_id)
 
-            storebook = StoreBook1(
+            storebook = StoreBook(
                 stock_level=stock_level,
                 book_info=book_json_str,
                 fk_store_id=store_id,
@@ -93,7 +36,6 @@ class mySeller(DataBase):
             return 530, "{}".format(str(e))
         return 200, "ok"
 
-#添加级别，设置主键
     def add_stock_level(self, user_id: str, store_id: str, book_id: str, add_stock_level: int):
         try:
             if not self.user_id_exist(user_id):
@@ -105,9 +47,9 @@ class mySeller(DataBase):
 
             session = self.DbSession()
             result = session.query(
-                StoreBook1
+                StoreBook
             ).filter(
-                StoreBook1.fk_store_id == store_id, StoreBook1.fk_book_id == book_id
+                StoreBook.fk_store_id == store_id, StoreBook.fk_book_id == book_id
             ).first()
 
             if result:
@@ -122,7 +64,6 @@ class mySeller(DataBase):
             return 530, "{}".format(str(e))
         return 200, "ok"
 
-#创建商店
     def create_store(self, user_id: str, store_id: str) -> tuple((int, str)):
         try:
             if not self.user_id_exist(user_id):
@@ -131,8 +72,8 @@ class mySeller(DataBase):
                 return error.error_exist_store_id(store_id)
 
             session = self.DbSession()
-            newStore = Store1(store_id=store_id)
-            newStoreUser = StoreUser1(fk_store_id=store_id, fk_user_id=user_id)
+            newStore = Store(store_id=store_id)
+            newStoreUser = StoreUser(fk_store_id=store_id, fk_user_id=user_id)
             session.add(newStore)
             session.add(newStoreUser)
             session.commit()
@@ -144,7 +85,6 @@ class mySeller(DataBase):
             return 530, "{}".format(str(e))
         return 200, "ok"
 
-#成功寄件
     def deliver_goods(self, order_id, store_id) -> tuple((int, str)):
         try:
             if not self.store_id_exist(store_id):
@@ -152,10 +92,10 @@ class mySeller(DataBase):
 
             session = self.DbSession()
             order = session.query(
-                Order1
+                Order
             ).filter(
-                Order1.order_id == order_id,
-                Order1.fk_store_id == store_id
+                Order.order_id == order_id,
+                Order.fk_store_id == store_id
             ).first()
             if order is None:
                 session.close()
