5b7dbb7efe9669915b21c7a2950cf8eeb765e334
Liao <86995945+duliaojason@users.noreply.github.com>
2022-12-08 11:11:39 +0800

    Add files via upload

--- bookstore/be/model/buyer.py
+++ bookstore/be/model/buyer.py
@@ -3,14 +3,14 @@ import uuid
 import json
 import logging
 from be.model import error
-
+#自己添加的包
 import datetime
-from be.model.db import DB
-from be.model.db import User, Book, StoreBook, StoreUser, Order, OrderBook
+from be.model.db import DataBase
+from be.model.db import User1, Book1, StoreBook1, StoreUser1, Order1, OrderBook1
 
-class myBuyer(DB):
+class Buyer(DataBase):
     def __init__(self):
-        DB.__init__(self)
+        DataBase.__init__(self)
 
     def new_order(self, user_id: str, store_id: str, id_and_count: list[(str, int)]) -> tuple((int, str, str)):
         order_id = ""
@@ -20,32 +20,31 @@ class myBuyer(DB):
             if not self.store_id_exist(store_id):
                 return error.error_non_exist_store_id(store_id) + (order_id, )
             order_id = "{}_{}_{}".format(user_id, store_id, str(uuid.uuid1()))
-            
-            new_order = Order(
-                    order_id = order_id,
-                    time = datetime.datetime.now(),
-                    total_price = 0,
-                    status = 0,
-                    fk_buyer_id = user_id,
-                    fk_store_id = store_id,
-                )
+
+            new_order = Order1(
+                order_id=order_id,
+                time=datetime.datetime.now(),
+                total_price=0,
+                status=0,
+                fk_buyer_id=user_id,
+                fk_store_id=store_id,
+            )
             details = []
             session = self.DbSession()
             for book_id, count in id_and_count:
                 result = session.query(
-                    Book.price,
-                    StoreBook
-                    ).join(
-                        StoreBook,
-                        Book.id == StoreBook.fk_book_id
-                    ).filter(
-                        Book.id == book_id,
-                        StoreBook.fk_store_id == store_id
-                    ).first()
+                    Book1.price,
+                    StoreBook1
+                ).join(
+                    StoreBook1,
+                    Book1.id == StoreBook1.fk_book_id
+                ).filter(
+                    Book1.id == book_id,
+                    StoreBook1.fk_store_id == store_id
+                ).first()
                 if result is None:
                     session.close()
-                    return error.error_non_exist_book_id(book_id) + (order_id, )
-
+                    return error.error_none_exist_book_id(book_id)+(order_id,)
                 stock_level = result[1].stock_level
                 price = result[0]
 
@@ -57,19 +56,18 @@ class myBuyer(DB):
                 session.add(result[1])
                 new_order.total_price += price * count
                 details.append((count, price, book_id))
-                
+
             session.add(new_order)
 
             for d in details:
-                o_b = OrderBook(
-                    num = d[0],
-                    price = d[1],
-                    fk_book_id = d[2],
-                    fk_order_id = order_id
+                o_b = OrderBook1(
+                    num=d[0],
+                    price=d[1],
+                    fk_book_id=d[2],
+                    fk_order_id=order_id
                 )
                 session.add(o_b)
-                
-            # 所有事务结束后，提交
+
             session.commit()
             session.close()
             del details
@@ -81,14 +79,15 @@ class myBuyer(DB):
             return 530, "{}".format(str(e)), ""
         return 200, "ok", order_id
 
+    #支付
     def payment(self, user_id: str, password: str, order_id: str) -> tuple((int, str)):
         try:
             session = self.DbSession()
             order = session.query(
-                    Order
-                ).filter(
-                    Order.order_id == order_id
-                ).first()
+                Order1
+            ).filter(
+                Order1.order_id == order_id
+            ).first()
 
             if order is None:
                 session.close()
@@ -103,10 +102,10 @@ class myBuyer(DB):
                 return error.error_authorization_fail()
 
             buyer = session.query(
-                    User
-                ).filter(
-                    User.user_id == buyer_id
-                ).first()
+                User1
+            ).filter(
+                User1.user_id == buyer_id
+            ).first()
             if buyer is None:
                 session.close()
                 return error.error_non_exist_user_id(buyer_id)
@@ -117,10 +116,10 @@ class myBuyer(DB):
                 return error.error_authorization_fail()
 
             seller_id = session.query(
-                    StoreUser.fk_user_id
-                ).filter(
-                    StoreUser.fk_store_id == store_id
-                ).first()
+                StoreUser1.fk_user_id
+            ).filter(
+                StoreUser1.fk_store_id == store_id
+            ).first()
             seller_id = seller_id[0]
 
             if seller_id is None:
@@ -131,15 +130,14 @@ class myBuyer(DB):
                 return error.error_not_sufficient_funds(order_id)
 
             seller = session.query(
-                    User
-                ).filter(
-                    User.user_id == seller_id
-                ).first()
+                User1
+            ).filter(
+                User1.user_id == seller_id
+            ).first()
 
             cur_time = datetime.datetime.now()
             cur_time = datetime.date(cur_time.year, cur_time.month, cur_time.day)
-            if (cur_time - order.time).days > 3:
-                # 支付超时(3天)
+            if (cur_time - order.time).days >= 1:
                 order.status = -1
                 session.add(order)
                 session.commit()
@@ -161,15 +159,16 @@ class myBuyer(DB):
             return 530, "{}".format(str(e))
         return 200, "ok"
 
+    #用户添加存款
     def add_funds(self, user_id, password, add_value) -> tuple((int, str)):
         try:
             session = self.DbSession()
             user = session.query(
-                    User
-                ).filter(
-                    User.user_id == user_id
-                ).first()
-                
+                User1
+            ).filter(
+                User1.user_id == user_id
+            ).first()
+
             if user is None:
                 session.close()
                 return error.error_authorization_fail()
@@ -181,92 +180,6 @@ class myBuyer(DB):
             session.add(user)
             session.commit()
             session.close()
-            
-        except sqlite.Error as e:
-            return 528, "{}".format(str(e))
-        except BaseException as e:
-            return 530, "{}".format(str(e))
-        return 200, "ok"
-
-    def search_orders(self, user_id, password) -> tuple((int, str, [])):
-        try:
-            cur_time = datetime.datetime.now()
-            cur_time = datetime.date(cur_time.year, cur_time.month, cur_time.day)
-
-            session = self.DbSession()
-            user = session.query(
-                    User
-                ).filter(
-                    User.user_id == user_id
-                ).first()
-            if user is None:
-                session.close()
-                return error.error_authorization_fail()
-            if user.password != password:
-                session.close()
-                return error.error_authorization_fail()
-
-            orders = []
-            result = session.query(
-                    Order
-                ).filter(
-                    Order.fk_buyer_id == user_id
-                ).all()
-            for od in result:
-                # 支付超时
-                if (cur_time - od.time).days > 3:
-                    od.status = -1
-                    session.add(od)
-                    continue
-                order = {
-                    "order_id": od.order_id,
-                    "time": od.time,
-                    "total_price": od.total_price,
-                    "status": od.status
-                }
-                orders.append(order)
-
-            session.commit()
-            session.close()
-
-        except sqlite.Error as e:
-            return 528, "{}".format(str(e)), []
-        except BaseException as e:
-            return 530, "{}".format(str(e)), []
-        return 200, "ok", orders
-
-    def cancel_order(self, order_id, user_id, password) -> tuple((int, str)):
-        try:
-            session = self.DbSession()
-            user = session.query(
-                    User
-                ).filter(
-                    User.user_id == user_id
-                ).first()
-            if user is None:
-                session.close()
-                return error.error_authorization_fail()
-            if user.password != password:
-                session.close()
-                return error.error_authorization_fail()
-
-            order = session.query(
-                Order
-            ).filter(
-                Order.order_id == order_id,
-                Order.fk_buyer_id == user_id
-            ).first()
-            if order is None:
-                session.close()
-                return error.error_change_others_order(user_id, order_id)
-            if order.status != 0:
-                session.close()
-                return error.error_order_status_not_fit(order_id)
-
-            order.status = -1
-            session.add(order)
-            session.commit()
-            session.close()
 
         except sqlite.Error as e:
             return 528, "{}".format(str(e))
@@ -274,41 +187,3 @@ class myBuyer(DB):
             return 530, "{}".format(str(e))
         return 200, "ok"
 
-    def take_delivery(self, order_id, user_id, password) -> tuple((int, str)):
-        try:
-            session = self.DbSession()
-            user = session.query(
-                    User
-                ).filter(
-                    User.user_id == user_id
-                ).first()
-            if user is None:
-                session.close()
-                return error.error_authorization_fail()
-            if user.password != password:
-                session.close()
-                return error.error_authorization_fail()
-
-            order = session.query(
-                Order
-            ).filter(
-                Order.order_id == order_id,
-                Order.fk_buyer_id == user_id
-            ).first()
-            if order is None:
-                session.close()
-                return error.error_change_others_order(user_id, order_id)
-            if order.status != 2:
-                session.close()
-                return error.error_order_status_not_fit(order_id)
-
-            order.status = 3
-            session.add(order)
-            session.commit()
-            session.close()
-
-        except sqlite.Error as e:
-            return 528, "{}".format(str(e))
-        except BaseException as e:
-            return 530, "{}".format(str(e))
-        return 200, "ok"
