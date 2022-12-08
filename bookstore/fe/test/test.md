83797d138a45b2a8df1ddc9e89bd252b7e8a9fda
Liao <86995945+duliaojason@users.noreply.github.com>
2022-12-08 13:28:12 +0800

    Delete bookstore directory

--- bookstore/fe/test/test.md
+++
@@ -1,1 +0,0 @@
-add functionality test here
--- bookstore/script/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export PATHONPATH=`pwd`
-coverage run --timid --branch --source fe,be --concurrency=thread -m pytest -v --ignore=fe/data
-coverage combine
-coverage report
-coverage html
